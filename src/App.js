9f022ea50a395ca86ebcd292b4155dbbfae378b8
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2021-10-25 12:10:11 +0200

    Created Simpsons Quote

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useState } from 'react';
+import DisplaySimpsonsQuote from './Components/DisplaySimpsons';
+
 import './App.css';
 
+const simpsonsQuote = 
+  {
+   quote: "Shoplifting is a victimless crime, like punching someone in the dark.",
+   character: "Nelson Muntz",
+   image : "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185",
+   characterDirection : "Left"
+  }
+
+
 function App() {
+  const [simpsons, setSimpsons] = useState(simpsonsQuote);
+  const getSimpsons = () => {
+
+    // Send the request
+  
+    axios
+  
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+  
+      // Extract the DATA from the received response
+  
+      .then((response) => response.data)
+  
+      // Use this data to update the state
+  
+      .then((data) => {
+  
+        setSimpsons(data[0]);
+      
+  
+      });
+  
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DisplaySimpsonsQuote simpsons={simpsons} />
+      <button type="button" onClick={getSimpsons}>Get Simpsons</button>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Components/DisplaySimpsons.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function DisplaySimpsonsQuote ({ simpsons }) {
+
+    return (
+  
+      simpsons && (
+  
+        <div className='DisplaySimpsonsQuote'>
+  
+
+        <img src={simpsons.image} />
+
+  
+          <ul>
+  
+            <li>Quote: {simpsons.quote}</li>
+  
+            <li>
+  
+              Character: {simpsons.character} {' '}
+  
+  
+            </li>
+  
+          </ul>
+  
+        </div>
+  
+      )
+  
+    );
+  
+  }
+  
+  
+  export default DisplaySimpsonsQuote;
